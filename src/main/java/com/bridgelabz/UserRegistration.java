2c5518917a6bafc0f6797f6959e447c84e454a47
ranirban60@gmail.com <ranirban60@gmail.com>
2022-08-18 16:45:19 +0530

    Added : Last name validation is added

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -9,11 +9,15 @@ public class UserRegistration {
     public static void main(String[] args) {
         boolean firstName = givenFirstName_IfValid_ReturnTrue("Ani");
         System.out.println("Is first name valid ? \n" + firstName);
-
+        boolean lastName = givenLastName_IfValid_ReturnTrue("Roy");
+        System.out.println("Is last name valid ? \n" + lastName);
     }
 
     // This method takes First Name which starts with Capital letter and has minimum of 3 characters
     private static boolean givenFirstName_IfValid_ReturnTrue(String validFirstname) {
         return Pattern.matches("^[A-Z][a-z]{2,}", validFirstname);
+    }
+    public static boolean givenLastName_IfValid_ReturnTrue(String validLastName) {
+        return Pattern.matches("^[A-Z][a-z]{2,}", validLastName);
     }
 }
