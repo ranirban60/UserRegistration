50b92250c8643104b0c6937fda1a4b6e73096168
ranirban60@gmail.com <ranirban60@gmail.com>
2022-08-18 17:06:07 +0530

    Added : Email validation is added

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,5 +1,5 @@
 /*
-User need to enter a valid Last Name
+User need to enter a valid email
  */
 package com.bridgelabz;
 import java.util.regex.Pattern;
@@ -10,13 +10,20 @@ public class UserRegistration {
         System.out.println("Is first name valid ? \n" + firstName);
         boolean lastName = givenLastName_IfValid_ReturnTrue("Roy");
         System.out.println("Is last name valid ? \n" + lastName);
+        boolean validUserEmail = givenEmail_IfValid_ReturnTrue("abc.xyz@bl.co.in");
+        System.out.println("Is user email valid ? \n" + validUserEmail);
     }
 
     // This method takes First Name which starts with Capital letter and has minimum of 3 characters
     private static boolean givenFirstName_IfValid_ReturnTrue(String validFirstname) {
         return Pattern.matches("^[A-Z][a-z]{2,}", validFirstname);
     }
+
     public static boolean givenLastName_IfValid_ReturnTrue(String validLastName) {
         return Pattern.matches("^[A-Z][a-z]{2,}", validLastName);
+    }
+
+    public static boolean givenEmail_IfValid_ReturnTrue(String validUserEmail) {
+        return Pattern.matches("^[a-z]+([_+.-]?[a-z0-9])*(@)(bl.co.)[a-z]{2}$", validUserEmail);
     }
 }
