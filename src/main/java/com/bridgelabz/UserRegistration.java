035dfe4fbd9352bcbe9ab4fe2f2b2467079e4e1e
ranirban60@gmail.com <ranirban60@gmail.com>
2022-08-18 17:16:45 +0530

    Added : Mobile Number validation is added

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,5 +1,5 @@
 /*
-User need to enter a valid email
+User need to enter a valid mobile number
  */
 package com.bridgelabz;
 import java.util.regex.Pattern;
@@ -12,6 +12,8 @@ public class UserRegistration {
         System.out.println("Is last name valid ? \n" + lastName);
         boolean validUserEmail = givenEmail_IfValid_ReturnTrue("abc.xyz@bl.co.in");
         System.out.println("Is user email valid ? \n" + validUserEmail);
+        boolean validUserMobileNum = givenMobileNum_IfValid_ReturnTrue("91 9919819801");
+        System.out.println("Is user mobile number valid ? \n" + validUserMobileNum);
     }
 
     // This method takes First Name which starts with Capital letter and has minimum of 3 characters
@@ -25,5 +27,9 @@ public class UserRegistration {
 
     public static boolean givenEmail_IfValid_ReturnTrue(String validUserEmail) {
         return Pattern.matches("^[a-z]+([_+.-]?[a-z0-9])*(@)(bl.co.)[a-z]{2}$", validUserEmail);
+    }
+
+    public static boolean givenMobileNum_IfValid_ReturnTrue(String validUserMobileNum) {
+        return Pattern.matches("^[]0-9]{2}[\s][1-9][0-9]{9}", validUserMobileNum);
     }
 }
